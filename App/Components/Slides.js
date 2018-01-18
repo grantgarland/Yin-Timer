94f07760b1395a7d6281dfec2241a45a0e1208a6
Grant <gcg4wg@virginia.edu>
2018-01-17 21:32:22 -0500

    implement rn-swiper

--- App/Components/Slides.js
+++ App/Components/Slides.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import { View, Image, ScrollView, Dimensions } from 'react-native';
+import { View, Image } from 'react-native';
+import Swiper from 'react-native-swiper';
 
 import Styles from '../Themes/masterStyles';
-const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
   renderSlides(){
     return this.props.data.map((slide) => {
       return (
-        <View
-          key={slide.index}
-          style={slideStyle}
-        >
+        <View key={slide.index} style={Styles.screen.mainContainer} >
           <Image
             style={Styles.screen.backgroundImage}
             source={slide.image}
-          > 
+          >
           </Image>
         </View>
       )
@@ -23,21 +20,11 @@ class Slides extends Component {
   }
   render() {
     return (
-      <ScrollView
-        horizontal
-        pagingEnabled
-      >
+      <Swiper showsButtons={true} loop={false}>
         {this.renderSlides()}
-      </ScrollView>
+      </Swiper>
     );
   }
-}
-
-const slideStyle = {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  width: SCREEN_WIDTH
 }
 
 export default Slides;
